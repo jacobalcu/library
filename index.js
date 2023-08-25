59ac4da4146b2ce4e121cd4de2e9a40bb06ad096
jalcumb <101678531+jacobalcu@users.noreply.github.com>
2023-08-25 14:51:06 -0400

    crude version displaying in html

--- index.js
+++ index.js
@@ -38,3 +38,43 @@ class Library {
     return res;
   }
 }
+
+const lib = new Library();
+const book1 = new Book(400, "Jane", "Doe", false);
+const book2 = new Book(300, "Jon", "Doe", false);
+const book3 = new Book(200, "Jack", "Doe", false);
+lib.addBook(book1);
+lib.addBook(book2);
+lib.addBook(book3);
+
+const booksGrid = document.getElementById("book-grid");
+
+const updateBooks = () => {
+  removeBooks();
+  for (let book of lib.books) {
+    createBookCard(book);
+  }
+};
+
+const removeBooks = () => {
+  booksGrid.innerHTML = "";
+};
+
+const createBookCard = (book) => {
+  const bookCard = document.createElement("div");
+  const name = document.createElement("p");
+  const author = document.createElement("p");
+  const pages = document.createElement("p");
+
+  name.textContent = book.name;
+  author.textContent = book.author;
+  pages.textContent = book.pages;
+
+  bookCard.appendChild(name);
+  bookCard.appendChild(author);
+  bookCard.appendChild(pages);
+
+  booksGrid.appendChild(bookCard);
+};
+
+updateBooks();
